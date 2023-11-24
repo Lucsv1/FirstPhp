90ffba6c9d222adbe7cc3b7730d13590931f7e7c
Lucas <lcasgalindo@gmail.com>
2023-11-24 15:52:29 -0300

    Usando caracteres especias

--- strings.php
+++ strings.php
@@ -7,4 +7,5 @@ $nome = "Lucas";
 
 $email = "lcasgalindo@gmail.com";
 
- echo "Meu nome é $nome e minha idade é $idade e meu email é $email";
+echo "ola mundo" . PHP_EOL;
+echo "Meu nome é $nome e minha idade é $idade e meu email é $email";
